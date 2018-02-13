ed6000991d02fba3cfbc6d0d829e8a2a0593eab0
xyc19970716 <465140038@qq.com>
2018-02-13 17:12:28 +0800

    optimize

--- src/com/Xieyuchen/enery/Enery.java
+++ src/com/Xieyuchen/enery/Enery.java
@@ -7,11 +7,17 @@ import java.awt.Image;
 
 //障碍物的抽象父类  
 public abstract class Enery {
+    public static final int STATUS_BIG = 1;
     public int Id;
     public int x,y;
     public int width,height;
     public Image img;
     public String name;
+    public boolean eatMushroom = false;
+    public int eatId;
+    public int status;
+    public int xspeed;
+    public int yspeed;
 
     public Enery(int x, int y, Image img,String name) {
         this.x = x;
